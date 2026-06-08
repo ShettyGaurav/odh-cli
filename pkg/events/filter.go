53c926156d98ad6c3ffdbfe321708f47ecf174ee
Shetty Gaurav Jagadeesha Shetty <sheshett@sheshett-thinkpadp1gen4i.bengluru.csb>
2026-06-08 10:58:19 +0530

    feat(events): add --component flag to filter events by ODH component

--- pkg/events/filter.go
+++ pkg/events/filter.go
@@ -3,11 +3,16 @@ package events
 import (
 	"cmp"
 	"context"
+	"fmt"
 	"slices"
 	"time"
 
 	"github.com/opendatahub-io/opendatahub-operator/pkg/clusterhealth"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/opendatahub-io/odh-cli/pkg/resources"
 	clierrors "github.com/opendatahub-io/odh-cli/pkg/util/errors"
 )
 
@@ -77,4 +82,87 @@ func sortEventsByTime(events []clusterhealth.EventInfo) {
 	slices.SortStableFunc(events, func(a, b clusterhealth.EventInfo) int {
 		return cmp.Compare(b.LastTime.UnixNano(), a.LastTime.UnixNano())
 	})
+}
+
+// filterEventsByComponent filters events to only those related to a component.
+// It looks up each event's InvolvedObject and checks for the component label.
+func (c *Command) filterEventsByComponent(ctx context.Context, events []clusterhealth.EventInfo) []clusterhealth.EventInfo {
+	targetLabel := resources.GetComponentLabelValue(c.Component)
+	labelCache := make(map[string]bool)
+
+	var filtered []clusterhealth.EventInfo
+
+	for _, event := range events {
+		cacheKey := fmt.Sprintf("%s/%s/%s/%s", targetLabel, event.Namespace, event.Kind, event.Name)
+
+		hasLabel, found := labelCache[cacheKey]
+		if !found {
+			hasLabel = c.checkObjectHasComponentLabel(ctx, event.Namespace, event.Kind, event.Name, targetLabel)
+			labelCache[cacheKey] = hasLabel
+		}
+
+		if hasLabel {
+			filtered = append(filtered, event)
+		}
+	}
+
+	return filtered
+}
+
+// checkObjectHasComponentLabel checks if an object has the component label.
+func (c *Command) checkObjectHasComponentLabel(ctx context.Context, namespace, kind, name, labelValue string) bool {
+	gvr := kindToGVR(kind)
+	if gvr.Resource == "" {
+		return false
+	}
+
+	var obj interface {
+		GetLabels() map[string]string
+	}
+
+	if namespace != "" {
+		unstr, err := c.Client.Dynamic().Resource(gvr).Namespace(namespace).Get(ctx, name, metav1.GetOptions{})
+		if err != nil {
+			return false
+		}
+
+		obj = unstr
+	} else {
+		unstr, err := c.Client.Dynamic().Resource(gvr).Get(ctx, name, metav1.GetOptions{})
+		if err != nil {
+			return false
+		}
+
+		obj = unstr
+	}
+
+	labels := obj.GetLabels()
+
+	return labels[resources.ComponentLabelKey] == labelValue
+}
+
+// kindToGVR maps Kubernetes Kind to GroupVersionResource.
+func kindToGVR(kind string) schema.GroupVersionResource {
+	switch kind {
+	case "Pod":
+		return resources.Pod.GVR()
+	case "Deployment":
+		return resources.Deployment.GVR()
+	case "ReplicaSet":
+		return resources.ReplicaSet.GVR()
+	case "StatefulSet":
+		return resources.StatefulSet.GVR()
+	case "DaemonSet":
+		return resources.DaemonSet.GVR()
+	case "Service":
+		return resources.Service.GVR()
+	case "ConfigMap":
+		return resources.ConfigMap.GVR()
+	case "Secret":
+		return resources.Secret.GVR()
+	case "Job":
+		return resources.Job.GVR()
+	default:
+		return schema.GroupVersionResource{}
+	}
 }
